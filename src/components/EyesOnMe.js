ac3effe75abd9b1b0ea2f49ff108e4d73b1be622
nohaderf <freda.hon@gmail.com>
2021-01-11 21:15:25 -0500

    commit tree 05c68a13838ad176a7bb704e22097cac6274131f

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function focus(){
+    console.log("Good!")
+}
+
+function blur(){
+    console.log("Hey! Eyes on me!")
+}
+function EyesOnMe(){
+    return(
+        <button onClick={focus} onClick={blur}>Eyes on Me</button>
+    )
+}
+
+export default EyesOnMe
